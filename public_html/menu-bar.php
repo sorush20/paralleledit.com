065d7010c375c91742045f58571a576116dbf022
sorush20 <sorush.nazari@gmail.com>
2019-03-18 17:25:36 +0000

    Menu Bar
    Menu Bar and Video addition

---
+++ public_html/menu-bar.php
@@ -0,0 +1,10 @@
+<!DOCUTYPE HTML>
+<div class="menu-bar">
+
+<a href="about.html">   About   	</a>
+<a href="contact.html">   Contact   </a>
+<a href="showreel.html">	Showreel	</a>
+<a href="location.html">	Location	</a>
+
+</div>
+</html>
